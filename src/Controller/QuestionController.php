eba2ed5bbcce082f548293a16d218381246d490d
Krystian Grzechnik <grzechnik.krystian@gmail.com>
2021-07-30 15:47:33 +0200

    Chapter 09

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -3,15 +3,17 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
     /**
      * @Route("/")
      */
-    public function homepage()
+    public
+    function homepage()
     {
         return new Response('What a bewitching controller we have conjured!');
     }
@@ -19,11 +21,19 @@ class QuestionController
     /**
      * @Route("/questions/{slug}")
      */
-    public function show($slug)
+    public
+    function show($slug)
     {
-        return new Response(sprintf(
-            'Future page to show question "%s"!',
-            ucwords(str_replace('-',' ',$slug))
-        ));
+        $answers =[
+          'Make sure your cat is sitting..',
+          'Honestly, i ike furry shoes better than..',
+          'Maybe.. try saying the spell backwards'
+
+        ];
+
+        return $this->render('question/show.html.twig', [
+            'question' => ucwords(str_replace('-',' ',$slug)),
+            'answers' => $answers,
+        ]);
     }
 }
